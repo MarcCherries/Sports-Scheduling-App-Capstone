92d934965373f0b04c8516aebdfd6047c5140ff9
Chris Creamer <chrisc1983@gmail.com>
2022-05-03 12:28:27 -0700

    was able to complete the models for all 5 apps and tested by adding entries via django admin site

--- backend/comments/models.py
+++ backend/comments/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 from users.models import User  
+from events.models import Event
 
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-   
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    comment_text = models.CharField(max_length=255)
--- backend/locations/models.py
+++ backend/locations/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 class Location(models.Model):
     location_name = models.CharField(max_length=100)
-    location_coordinates = models.DecimalField(max_digits=20, decimal_places=4)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
     location_info = models.CharField(max_length=255)
